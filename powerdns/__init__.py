a19500bc321bb4caef4307ca38b295ad5f98b2b8
Denis 'jawa' Pompilio <dpompilio@vente-privee.com>
2018-06-19 15:01:39 +0200

    Release of version 0.2.0
    * Modified setup.py for wheels creation
    * PyLint and documentation

--- powerdns/__init__.py
+++ powerdns/__init__.py
@@ -28,7 +28,7 @@ from .interface import PDNSEndpoint, RRSet
 
 
 #: Current version of the package as :class:`str`.
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 LOG_LEVELS = [
     logging.ERROR,
