d48641fa5e783413b912744c11df14732c380c8d
Denis 'jawa' Pompilio <dpompilio@veepee.com>
2021-06-07 16:48:42 +0200

    feat: release version 1.0.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ import setuptools
 
 if __name__ == '__main__':
     readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
-    release = "0.2.5"
+    release = "1.0.0"
     setuptools.setup(
         name="python-powerdns",
         version=release,
