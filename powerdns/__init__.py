a023909540190a505294de47033c78edc13fde06
Denis 'jawa' Pompilio <denis.pompilio@gmail.com>
2021-06-15 15:49:40 +0200

    feat: release of version 2.0.0

--- powerdns/__init__.py
+++ powerdns/__init__.py
@@ -28,7 +28,7 @@ from .interface import PDNSEndpoint, RRSet, Comment
 
 
 #: Current version of the package as :class:`str`.
-__version__ = "0.2.5"
+__version__ = "2.0.0"
 
 LOG_LEVELS = [
     logging.ERROR,
